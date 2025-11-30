428b615b4535157c6634e382a21d5edc3e2eb2a4
Ian Souza <ianfmsouza@gmail.com>
2025-11-30 12:15:19 -0300

    chore: tipando response "users"

--- src/routes/black/index.tsx
+++ src/routes/black/index.tsx
@@ -5,18 +5,19 @@ export const Route = createFileRoute('/black/')({
   loader: async () => {
     const response = await fetch('http://localhost:3000/black/api/users')
     const data = await response.json()
-    return data
+
+    return data as {users: string[]}
   }
 })
 
 function RouteComponent() {
-  const data = Route.useLoaderData()
+  const {users} = Route.useLoaderData()
 
   return (
     <div className='text-white bg-zinc-950 min-h-screen p-8 flex flex-col items-center justify-center'>
       <h1 className='text-4xl font-medium mb-2'>Hello "/black/"!</h1>
 
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   )
 }
